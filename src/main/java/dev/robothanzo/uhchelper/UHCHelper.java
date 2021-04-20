f75bd716e0ffe61997daf28d922e9be920e4e6de
RobotHanzo <nathanlee302@outlook.com>
2021-04-20 19:03:21 +0800

    Automatically deletes world after server reboot

--- src/main/java/dev/robothanzo/uhchelper/UHCHelper.java
+++ src/main/java/dev/robothanzo/uhchelper/UHCHelper.java
@@ -4,16 +4,41 @@ import dev.robothanzo.uhchelper.commands.Autocomplete;
 import dev.robothanzo.uhchelper.commands.MainCommand;
 import dev.robothanzo.uhchelper.modes.RegistrationService;
 import io.github.thebusybiscuit.cscorelib2.config.Config;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.logging.Level;
 
 public final class UHCHelper extends JavaPlugin {
     public Config cfg;
 
+    private boolean deleteFolder(File worldDir){
+        File[] allContents = worldDir.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFolder(file);
+            }
+        }
+        return worldDir.delete();
+    }
+
     @Override
     public void onEnable() {
         cfg = new Config(this);
+
+        if (cfg.getBoolean("settings.cleanup-world") && Bukkit.getWorld("world") == null) {
+            getLogger().log(Level.WARNING, "Deleting the previous world...");
+            if (deleteFolder(new File("world"))){
+                getLogger().log(Level.INFO, "Deleted overworld");
+            }
+            if (deleteFolder(new File("world_nether"))){
+                getLogger().log(Level.INFO, "Deleted nether");
+            }
+            if (deleteFolder(new File("world_the_end"))){
+                getLogger().log(Level.INFO, "Deleted the end");
+            }
+        }
 
         new RegistrationService(this);
 
